2d28e04305f0e1b0a12cbbcd62b512c88ea6cea9
semucho12 <jerome.semucho@strathmore.edu>
2025-11-13 09:05:57 +0300

    Added booking page HTML and styling

--- SystemB/bookings/urls.py
+++ SystemB/bookings/urls.py
@@ -4,6 +4,6 @@ from . import views
 app_name = "bookings"  # important if using namespace
 
 urlpatterns = [
-    path('create/', views.create_booking, name='create_booking'),
+    path('create/<int:destination_id>/', views.create_booking, name='create_booking'),
 ]
 
